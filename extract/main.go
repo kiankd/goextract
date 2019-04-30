da0375f3ebb35ba0c07287a07fccf8502562c103
Kian Kenyon-Dean <kiankd@gmail.com>
2019-04-30 11:39:08 -0400

    changed directories

--- extract/main.go
+++ extract/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"strings"
 
 	"github.com/pkg/profile"
 )
@@ -13,9 +15,35 @@ func LoadSampleWords() [][]string {
 }
 
 func main() {
-	// defer profile.Start().Stop()
-	defer profile.Start(profile.MemProfile).Stop()
+	var path string
+	flag.StringVar(&path, "extract", "../data/sample.txt.gz",
+		"path to the target gz file we will be extracting")
+	debug := flag.Bool("debug", false,
+		"whether to run a debug profiler")
+	cpuProfile := flag.Bool("pcpu", false,
+		"whether to do CPU profiling (RAM is default)")
+	flag.Parse()
+
+	// TODO: pass to the logger all args and log them.
 	l := ConstructLogger("")
+
+	// Very beginning - ensure the file exists, and parse if there are multiple.
+	nPaths := strings.Count(path, " ") + 1
+	paths := make([]string, nPaths)
+	for i, p := range strings.Split(path, " ") {
+		paths[i] = p
+		fmt.Println(p)
+	}
+	return
+
+	// First check if we are doing debugging stuff.
+	if *debug {
+		if *cpuProfile {
+			defer profile.Start().Stop()
+		} else {
+			defer profile.Start(profile.MemProfile).Stop()
+		}
+	}
 
 	// allWords := ReadParseGz("/Users/kiankd/sentpiece/spm-parsed/spm-parsed00.gz", &l)
 	// u, _, _ := FullUnigramExtraction(&allWords, 50000, &l)
