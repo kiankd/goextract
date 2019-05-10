723ed3abc861c2900ea7ad8818fa8b8a9804325e
Kian Kenyon-Dean <kiankd@gmail.com>
2019-05-10 13:18:54 -0400

    removed unnecessary script

--- extract/run_all.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-while read p; do
-	pref=$(echo $p | cut -d '/' -f 6)	
-	./extract -e $p -C ../data/coocsW10/$pref.cooc -U ../data/unigrams/merged.unigram -v 50000 -w 10 -option cooc
-	gzip ../data/coocsW10/$pref.cooc
-	exit 0
-done<$1
-
